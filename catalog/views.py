1ee6c89f1dad06307a7cb926aee62c445789fd06
Valentin Melnikov <switch4xd@gmail.com>
2023-01-05 20:38:51 +0600

    Add borroved books for library worker

--- catalog/views.py
+++ catalog/views.py
@@ -47,10 +47,18 @@ class AuthorDetailView(DetailView):
     model = Author
 
 
-class LoanedBooksByUserListView(LoginRequiredMixin,ListView):
+class LoanedBooksByUserListView(LoginRequiredMixin, ListView):
     model = BookInstance
-    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
     paginate_by = 10
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class AllBorrowedBooksListView(LoginRequiredMixin, ListView):
+    model = BookInstance
+    template_name = 'catalog/all_borrowed_books.html'
+
+    def get_queryset(self):
+        return BookInstance.objects.all().filter(status__exact='o').order_by('due_back')
