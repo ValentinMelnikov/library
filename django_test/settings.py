93df690cd1ada5937986e2bc517945f152d9a51f
Valentin Melnikov <switch4xd@gmail.com>
2023-01-05 19:52:41 +0600

    Add html files registrations

--- django_test/settings.py
+++ django_test/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+LOGIN_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
