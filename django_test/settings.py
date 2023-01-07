9ccc9aabc23a58a1b15e28268b30352c32171572
Valentin Melnikov <switch4xd@gmail.com>
2023-01-07 21:47:31 +0600

    Added fix secret

--- django_test/settings.py
+++ django_test/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-lc2&=r^eg-=iq&92@8vdyy^kgno0+*7+%_lh%)pmg^_iey&v*5')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+                            'django-insecure-lc2&=r^eg-=iq&92@8vdyy^kgno0+*7+%_lh%)pmg^_iey&v*5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
